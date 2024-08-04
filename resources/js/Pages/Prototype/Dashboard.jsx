c8da1798bfc2db082adb26a6797caa96fe6cddc0
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-08-04 15:51:34 +0700

    feat: slicing sidebar

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,4 +1,4 @@
-import Authenticated from "@/Layouts/Authenticated/index.jsx";
+import Authenticated from "@/Layouts/Authenticated/index.jsx/Index.jsx";
 // import "../../../css/sidebar.css";
 
 export default function Dashboard() {
